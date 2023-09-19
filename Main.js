fb0495ab988392855ce0b81faf69b1a97660d453
Daniel Celiz Juarez <124106671+DaniCJ2233@users.noreply.github.com>
2023-09-19 07:58:40 -0300

    first commit

---
+++ Main.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <div className="bloque1"></div>
+      <div className="container-flex">
+        <button className="container-item">Mis proyectos</button>
+        <button className="container-item">Mis estudios</button>
+        <button className="container-item">Contacto</button>
+      </div>
+      <div className="contorno-blanco"> </div>
+      <div className="contorno-blanco2"> </div>
+      <h1 className="h1-1">
+        <p className="p1-1">Full Stack Developer</p>
+      </h1>
+       <div className="p1-3">
+        <p className="p1-3">Me gusta ofrecer experiencias de calidad, trabajos solidos y profesionales.</p>
+      </div>
+      <h1 className="h2-1">
+        La satisfaccion y la comodida del usuario es lo mas importante.
+      </h1>
+     
+    </div>
+  );
+};
+
+export default Main;
